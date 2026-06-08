0f1ff83b5633fe51284cd96034d4b1aab578a6db
Otavio Machado <otavio100206@gmail.com>
2026-06-08 18:44:46 -0300

    implement Investidor class with attributes and constructor

--- Investidor.java
+++ Investidor.java
@@ -1,3 +1,11 @@
 public class Investidor {
-    
+    int id;
+    String nome;
+    String email;
+
+    public Investidor(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
 }
