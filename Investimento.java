7d63bfa6406364763d998f94b403101f45160eff
Lorenzo Debiasi <136204885+snoidoin@users.noreply.github.com>
2026-06-27 19:01:30 -0300

    Investimento.java

--- Investimento.java
+++ Investimento.java
@@ -1,30 +1,61 @@
 public class Investimento {
-    Startup startup;
-    double valorCaptado;
+    private int id;
+    private double valor;
+    private double percentualParticipacao;
+    private Startup startup;
 
-    public Investimento(Startup startup, double valorCaptado){
+    public Investimento(int id, double valor, double percentualParticipacao, Startup startup) {
+        this.id = id;
+        this.valor = valor;
+        this.percentualParticipacao = percentualParticipacao;
         this.startup = startup;
-        this.valorCaptado = valorCaptado;
+    }
+
+    //etters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getPercentualParticipacao() {
+        return percentualParticipacao;
+    }
+
+    public void setPercentualParticipacao(double percentualParticipacao) {
+        this.percentualParticipacao = percentualParticipacao;
     }
 
     public Startup getStartup() {
         return startup;
     }
 
-    public double getValorCaptado() {
-        return valorCaptado;
-    }
-
     public void setStartup(Startup startup) {
         this.startup = startup;
     }
-    
-    public void setValorCaptado(double valorCaptado) {
-        this.valorCaptado = valorCaptado;
-    }
 
-    public void adicionarInvestimento(Startup startup, double valorCaptado){
-        this.startup = startup;
-        this.valorCaptado = valorCaptado;
+   
+    public String toString() {
+        String nomeInvestidorPrincipal = "N/A";
+        if (startup != null && startup.getInvestidor() != null) {
+            nomeInvestidorPrincipal = startup.getInvestidor().getNome();
+        }
+        return "Investimento [ID: " + id
+                + " | Valor: R$ " + String.format("%.2f", valor)
+                + " | Participacao: " + String.format("%.2f", percentualParticipacao) + "%"
+                + " | Startup: " + (startup != null ? startup.getNome() : "N/A")
+                + " | Investidor principal: " + nomeInvestidorPrincipal + "]";
     }
 }
+  
